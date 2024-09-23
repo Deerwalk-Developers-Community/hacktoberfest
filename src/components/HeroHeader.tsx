b6e4971b9d64ae68219ab32c1cd35bd4f6df6c33
Aashutosh Pudasaini <nepalidude3@gmail.com>
2024-09-23 15:43:58 +0545

    fix: register link and metadata

--- src/components/HeroHeader.tsx
+++ src/components/HeroHeader.tsx
@@ -11,8 +11,8 @@ interface HeroHeaderProp {
 
 const Header: React.FC<HeroHeaderProp> = ({ level, logo }) => {
   return (
-    <header className="flex relative flex-wrap gap-5 mt-12 mx-auto justify-between items-center w-full text-4xl text-center text-white max-w-[1681px] max-md:max-w-full">
-      {logo && <p className="w-32 md:w-48 lg:w-fit">Deertober</p>}
+    <header className="flex relative flex-wrap gap-5 mt-12 mx-auto justify-between items-center w-full text-4xl text-center text-white max-md:max-w-full">
+      {logo && <p className="w-32 md:w-32 lg:w-32">Deertober</p>}
       <HeroLevel
         className="w-32 text-lg md:text-xl lg:text-2xl md:w-48 lg:w-fit"
         level={level}
