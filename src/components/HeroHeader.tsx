c90511efed474129b1353222f1e82754c7ffc82e
chrisdvkta <chrisdevkota77@gmail.com>
2024-09-25 02:57:46 +0545

    fix:chore:

--- src/components/HeroHeader.tsx
+++ src/components/HeroHeader.tsx
@@ -3,20 +3,20 @@ import React from "react";
 // import Image from "next/image";
 // import logo from "@/assets/Logo.svg";
 // import Logo from "../modules/participatingSection/assets/Logo";
-import HeroLevel from './HeroLevel';
+// import HeroLevel from "./HeroLevel";
 interface HeroHeaderProp {
-  level: string;
+  level?: string;
   logo: boolean;
 }
 
-const Header: React.FC<HeroHeaderProp> = ({ level, logo }) => {
+const Header: React.FC<HeroHeaderProp> = ({ logo }) => {
   return (
-    <header className="flex relative flex-wrap gap-5 mt-12 mx-auto justify-between items-center w-full text-4xl text-center text-white max-md:max-w-full">
+    <header className="flex relative flex-wrap gap-5 mt-12 p-5 mx-auto justify-between items-center w-full text-4xl text-center text-white max-md:max-w-full">
       {logo && <p className="w-32 md:w-32 lg:w-32">Deertober</p>}
-      <HeroLevel
+      {/* <HeroLevel
         className="w-32 text-lg md:text-xl lg:text-2xl md:w-48 lg:w-fit"
         level={level}
-      />
+      /> */}
     </header>
   );
 };
