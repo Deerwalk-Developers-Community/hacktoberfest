ebc3ce0ec185f77774adec7e875eeda1558cc945
chrisdvkta <chrisdevkota77@gmail.com>
2024-09-25 02:50:50 +0545

    update:hero

--- src/components/HeroHeader.tsx
+++ src/components/HeroHeader.tsx
@@ -11,7 +11,7 @@ interface HeroHeaderProp {
 
 const Header: React.FC<HeroHeaderProp> = ({ level, logo }) => {
   return (
-    <header className="flex relative flex-wrap gap-5 mt-12 mx-auto justify-between items-center w-full text-4xl text-center text-white max-w-[1681px] max-md:max-w-full">
+    <header className="flex relative flex-wrap gap-5 mt-12 mx-auto justify-between items-center w-full text-4xl text-center text-white max-w-[1681px] max-md:max-w-full p-5">
       {logo && <p className="w-32 md:w-48 lg:w-fit">Deertober</p>}
       {/* <HeroLevel
         className="w-32 text-lg md:text-xl lg:text-2xl md:w-48 lg:w-fit"
