e572debb4ada6a5de72117230f5bac406eab5386
ghostuf <srijan102dahal@gmail.com>
2024-09-25 22:59:54 +0545

    fix spacing

--- src/modules/faq/page.tsx
+++ src/modules/faq/page.tsx
@@ -36,7 +36,7 @@ const Page = () => {
   return (
     <div className="relative flex flex-col justify-center font-bold bg-neutral-950 min-h-screen overflow-hidden pb-10 pt-0">
       <Stars className="absolute inset-0 w-fit h-full  min-h-[100vh] object-cover z-0" />{" "}
-      <div className="relative flex flex-col w-full lg:min-h-[1016px] max-md:min-h-[screen] max-md:px-5 max-md:max-w-full z-10">
+      <div className="relative flex flex-col w-full lg:min-h-[1016px]  max-md:px-5 max-md:max-w-full z-10">
         {" "}
         <Header level="3" logo={false} />
         <main className="flex  flex-col relative gap-5 lg:gap-10 items-center lg:items-start justify-start lg:justify-center self-center mt-16 ml-4 w-full max-w-[1654px] max-md:mt-10 max-md:max-w-full p-10 pt-0">
--- src/modules/hero/page.tsx
+++ src/modules/hero/page.tsx
@@ -9,7 +9,7 @@ const Page = () => {
     <div className="relative flex flex-col justify-center font-bold bg-neutral-950  overflow-hidden p-0 m-0">
       <Stars className="absolute inset-0 min-h-[100vh] w-fit lg:h-full object-cover z-0" />
       <Alien className="absolute top-0 right-96 w-[400px] h-[400px] lg:w-[600px] lg:h-[600px] min-h-[50vh] object-cover z-[1] transform translate-y-[20vh] translate-x-[10vw] hidden xl:block" />
-      <div className="relative flex flex-col w-full max-md:min-h-[100 dvh] max-md:px-5 max-md:max-w-full z-10 p-0 mb-20">
+      <div className="relative flex flex-col w-full  max-md:px-5 max-md:max-w-full z-10 p-0 mb-20">
         <Header logo={true} />
         <main className="mt-36 mb-0 pb-0">
           <Hero />
