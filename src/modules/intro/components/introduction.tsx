b2f95194a070b6c92aa63dd55acd82edcc657afa
ghostuf <srijan102dahal@gmail.com>
2024-09-25 18:37:06 +0545

    fix: svg error and build error

--- src/modules/intro/components/introduction.tsx
+++ src/modules/intro/components/introduction.tsx
@@ -6,11 +6,11 @@ const IntroductionSection: React.FC = () => {
       <div className="py-10 px-4 md:px-10 lg:pl-28 flex justify-center">
         <p className="text-[#FFBDDE] text-lg md:text-2xl lg:text-4xl max-w-full lg:max-w-[1250px] lg:h-[406px]">
           This year marks the 7th anniversary of DDC and <br />
-          we're calling on your support!
+          we&apos;re calling on your support!
           <br />
           <br />
-          Whether it's your first time participating or you have <br />
-          previously participated, it's almost time to hack out <br />
+          Whether it &apos; s your first time participating or you have <br />
+          previously participated, it &apos; s almost time to hack out <br />
           four pristine pull/merge requests as we continue our <br />
           month of support for open source.
         </p>
